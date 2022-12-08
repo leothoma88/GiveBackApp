6248339cae1f84d32956f90460b605de75fb4938
Darrell Thomas <darrellthomas@Darrells-MBP.hsd1.ga.comcast.net>
2022-12-07 19:22:54 -0500

    Peace

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import About from "./About"
+import Cards from "./Cards"
+import Donation from "./Donation"
+import Footer from "./Footer"
+import Header from "./Header"
+import Login from "./Login"
+import Main from "./Main"
+import Nav from "./Nav"
+import Profile from "./Profile"
+import Signup from "./Signup"
+
+export {About,Cards,Donation,Footer,Header,Login,Main,Nav,Profile,Signup}

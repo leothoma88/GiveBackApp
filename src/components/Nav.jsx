9c422ee661ed281dcc8916917d4991b4fa85d41a
Darrell Thomas <darrellthomas@Darrells-MBP.hsd1.ga.comcast.net>
2022-12-07 21:24:17 -0500

    Home,Header,Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className="flex justify-end">
+        <div className="space-x-4 space-y-4 mr-10">
+          <button type="button" className="inline-block px-9 py-2.5 bg-pink-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Primary</button>
+          <button type="button" className="inline-block px-9 py-2.5 bg-purple-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Secondary</button>
+        </div>
+      </div>
   )
 }
 

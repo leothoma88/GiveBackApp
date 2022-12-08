afccf18edb500b2ea00d31345ae867433ae92921
Kianna Reid <kiannareid@yahoo.com>
2022-12-08 10:20:05 -0500

    tailwind install

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import {About,Cards,Donation,Footer,Header,Login,Home,Nav,Profile,Signup} from "./components"
-import {Routes,Route,Navigate,withRouter} from 'react-router-dom'
-
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  About,
+  Cards,
+  Donation,
+  Footer,
+  Header,
+  Login,
+  Home,
+  Nav,
+  Profile,
+  Signup,
+} from "./components";
+import { Routes, Route, Navigate, withRouter } from "react-router-dom";
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
   return (
     <div className="App">
-      <Header/>
-        <Routes>
-          <Route path="/about" 
-          element= {<About/>}>
-          </Route>
-          <Route path="/" 
-          element= {<Home/>}>
-          </Route>
-          <Route path="/donation" element= {<Donation/>}>
-
-          </Route>
-            <Route path="/login" element= {<Login/>}>
-
-          </Route>
-            <Route path="/profile" element= {<Profile/>}>
-
-          </Route>
-            <Route path="/signup" element= {<Signup/>}>
-          </Route>
-        </Routes>
-        <Footer/>
-
+      <Header />
+      <Routes>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/donation" element={<Donation />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

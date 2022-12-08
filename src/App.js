a337c92a9b7ee3c8946b332b357ec2d0d2d035b7
pftho <pf.thomas97@gmail.com>
2022-12-07 20:52:30 -0500

    setup routes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ import {Routes,Route,Navigate,withRouter} from 'react-router-dom'
 
 function App() {
   return (
-    <React.Fragment>
-        <Header/>
+    <div className="App">
+      <Header/>
         <Routes>
-        <Route path="/about" element= {<About/>}/>
-        <Route path="/" element= {<Main/>}/>
-        <Route path="/donation" element= {<Donation/>}/>
-        <Route path="/login" element= {<Login/>}/>
-        <Route path="/profile" element= {<Profile/>}/>
-        <Route path="/signup" element= {<Signup/>}/>
+          <Route path="/about" 
+          element= {<About/>}>
+          </Route>
+          <Route path="/" 
+          element= {<Main/>}>
+          </Route>
+          <Route path="/donation" element= {<Donation/>}>
+
+          </Route>
+            <Route path="/login" element= {<Login/>}>
+
+          </Route>
+            <Route path="/profile" element= {<Profile/>}>
+
+          </Route>
+            <Route path="/signup" element= {<Signup/>}>
+          </Route>
         </Routes>
         <Footer/>
-
-    </React.Fragment>
+    </div>
   );
 }
 

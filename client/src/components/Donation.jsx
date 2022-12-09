2143351d7ca2cb5c4c7372b3200e83f4ab03f9d1
Cristina <cristinayunesm@gmail.com>
2022-12-08 21:24:09 -0500

    fixing onation grid

--- client/src/components/Donation.jsx
+++ client/src/components/Donation.jsx
@@ -3,8 +3,10 @@ import Card from './Cards'
 
 const Donation = () => {
   return (
-  <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+  <div className="grid grid-cols-1 p-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <div class="flex w-full">
     <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-left text-teal-500">My Donations</h1>
+    </div>
     <Card />
     <Card />
     <Card />

c07de6878b6a0900085e915a9d1499c2d365dbc1
Cristina <cristinayunesm@gmail.com>
2022-12-08 21:16:00 -0500

    pushing my donation page, cards and assets

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import photo from'../assets/charity-photo-test.avif'
 
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div className="rounded-lg overflow-hidden shadow-lg">
+      <div class="relative pb-48 overflow-hidden">
+      <img class="absolute inset-0 h-full w-full object-cover transition  hover:-translate-y-1 hover:scale-110" src={ photo } alt="" />
+      </div>
+
+      <div className="px-6 py-4">
+        <p className="font-bold text-xl mb-2">Charity Name</p>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+
+      <div className="px-6 pt-4 pb-2">
+        <button className="inline-block bg-teal-500 rounded-full px-5 py-2 text-sm font-semibold text-white mr-2 mb-2 hover:bg-teal-400">Donate</button>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Donation.jsx
+++ client/src/components/Donation.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Card from './Cards'
 
 const Donation = () => {
   return (
-    <div>Donation</div>
-  )
-}
+  <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-left text-teal-500">My Donations</h1>
+    <Card />
+    <Card />
+    <Card />
+</div>
+    );
+};
 
 export default Donation

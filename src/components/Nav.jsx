d2bce910eed5ac6ac054fd707d2500766f95fa9c
Darrell Thomas <darrellthomas@Darrells-MBP.hsd1.ga.comcast.net>
2022-12-08 19:42:41 -0500

    homepage with links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Nav = () => {
   return (
     <div className="flex justify-end">
         <div className="space-x-4 space-y-4 mr-10">
-          <button type="button" className="inline-block px-9 py-2.5 bg-pink-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Login/Sign Up</button>
-          <button type="button" className="inline-block px-9 py-2.5 bg-purple-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">About Us</button>
+          <Link className="inline-block px-9 py-2.5 bg-pink-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" to='/login'>Login/Sign Up</Link>
+          <Link className="inline-block px-9 py-2.5 bg-purple-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" to='/'>Home</Link>
+          <Link className="inline-block px-9 py-2.5 bg-purple-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" to='/profile'>Profile</Link>
+          <Link className="inline-block px-9 py-2.5 bg-purple-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" to='/about'>About Us</Link>
+    
         </div>
       </div>
   )
 }
 
 export default Nav
+
+
+

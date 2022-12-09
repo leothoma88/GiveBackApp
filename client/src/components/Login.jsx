4499ac0da64d6ba03abc6da09c4e58fd064f0990
Darrell Thomas <darrellthomas@Darrells-MBP.hsd1.ga.comcast.net>
2022-12-08 21:00:06 -0500

    push

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,8 +6,8 @@ const Login = () => {
     <div>
       <h1 className="text-3xl font-bold"> Login:</h1>
       <div className="flex content-center">
-        <div className="ml-64 p-5 h-full col-span-1 grid grid-cols-1 pl-80">
-          <div className="start bg-red-400 w-4/5 border-bg-black rounded-2xl">
+        <div className="flex ml-64 p-5 h-full col-span-1  pl-80">
+          <div className="flex start bg-red-400 w-4/5 border-bg-black rounded-2xl">
             <div className="card p-8 m-5 border-2 bg-red-300 rounded-2xl">
               <form className="form login-form">
                 <div className="form-group">

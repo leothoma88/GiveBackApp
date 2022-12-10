067d476ba390d9bf4c8bcf9ceeabc9ff3c2f0a42
pftho <pf.thomas97@gmail.com>
2022-12-10 13:22:01 -0500

    edited server to connect to graphql

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,7 @@ const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 
 // import schema from Donation.js
-const donationSchema = require("./Donation");
-
+const donationSchema = model('Donation').schema;
 const userSchema = new Schema(
   {
     username: {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,7 +31,6 @@ const typeDefs = gql`
   }
 
   type Query {
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
     me: User
   }
 
@@ -46,3 +45,5 @@ const typeDefs = gql`
 `;
 
 module.exports = typeDefs;
+
+// # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data

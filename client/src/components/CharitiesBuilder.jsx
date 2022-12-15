24f32c228eedab9afeb699df00f86a7ac419851b
Darrell Thomas <darrellthomas@Darrells-MacBook-Pro.local>
2022-12-15 16:41:55 -0500

    search button

--- client/src/components/CharitiesBuilder.jsx
+++ client/src/components/CharitiesBuilder.jsx
@@ -68,9 +68,9 @@ function CharitiesBuilder({setCharitiesToShow}) {
       return (
         <div >
           <Search  acceptSearches={ acceptSearches } charities={ reducedCharities }/>
-          <button className='text-white' onClick={() => {
+          <button className='text-white my-3 inline-block px-9 py-2.5 bg-pink-700 font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"' onClick={() => {
               handleSearch() 
-          }} >Do It!!!</button>
+          }} >Search Organizations</button>
 
         </div>
       );

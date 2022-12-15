efbb43be4764dd50abff7787baba73e237d01301
Kianna Reid <kiannareid@yahoo.com>
2022-12-15 10:00:31 -0500

    updating donate page

--- client/src/components/DonateHere.jsx
+++ client/src/components/DonateHere.jsx
@@ -6,27 +6,27 @@ import { FaPaypal } from "react-icons/fa";
 const DonateHere = () => {
   return (
     <div>
-      <div className="grid grid-cols-2 m-14">
+      <div className="card bg-red-900 grid grid-cols-2 m-14 rounded-lg">
         <form className="payment-form">
           <div className="select ">Select Payment Method</div>
-          <div className="flex flex-col payment-selection justify-items-center">
+          <div className="flex flex-col payment-selection m-10 justify-items-center">
             <button
               type="button"
-              className="credit-card border-y-2 border-white  bg-red-100"
+              className="credit-card border-y-2 border-black  bg-white"
             >
               <BsCreditCard />
               Credit Card
             </button>
             <button
               type="button"
-              className="bank border-y-0 border-white bg-red-100"
+              className="bank border-y-0 border-black bg-white"
             >
               <BsBank />
               Bank
             </button>
             <button
               type="button"
-              className="paypal border-y-2 border-white  bg-red-100"
+              className="paypal border-y-2 border-black bg-white"
             >
               <FaPaypal />
               PayPal
@@ -35,17 +35,18 @@ const DonateHere = () => {
         </form>
         <form className="donate-form">
           <div className="donateForm col-span-2 text-white">Donate Form</div>
-          <div className="flex flex-col">
+          <div className="flex flex-col m-10">
             <input
-              className="input w-1/2 rounded-full p-3"
+              className="input rounded-full p-3 "
               type="donation-amount"
               id="donation-amount"
             />
           </div>
-          <div className="donateBtns flex flex-row">
-            <div className="rounded-full p-3 bg-white m-5 ">$10</div>
-            <div className="rounded-full p-3 bg-white m-5 ">$20</div>
-            <div className="rounded-full p-3 bg-white m-5 ">$30</div>
+          <div className="donateBtns m-16 flex flex-row ">
+            <div className="rounded-full p-3 bg-white m-5 "> $10</div>
+            <div className="rounded-full p-3 bg-white m-5 "> $20</div>
+            <div className="rounded-full p-3 bg-white m-5 "> $30</div>
+            <div className="rounded-full p-3 bg-white m-5 "> Other</div>
           </div>
         </form>
       </div>
